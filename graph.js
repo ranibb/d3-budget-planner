856769690ce93fbb38b3c6c88829a50c3da3892f
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-10 21:27:50 +0300

    Legends

--- graph.js
+++ graph.js
@@ -18,12 +18,26 @@ const arcPath = d3.arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
-  const colour = d3.scaleOrdinal(d3['schemeSet3']);
+const colour = d3.scaleOrdinal(d3['schemeSet3']);
+
+// legend setup
+const legendGroup = svg.append('g')
+  .attr('transform', `translate(${dims.width + 40}, 10)`)
+
+const legend = d3.legendColor()
+  .shape('circle')
+  .shapePadding(10)
+  .scale(colour)
 
 // update function
 const update = (data) => {
   // update colour scale domain
   colour.domain(data.map(d => d.name));
+
+  // update and call legend
+  legendGroup.call(legend);
+  legendGroup.selectAll('text'). attr('fill', 'white');
+
   // Join enhanced (pie) data to path elements
   const paths = graph.selectAll('path')
     .data(pie(data))
