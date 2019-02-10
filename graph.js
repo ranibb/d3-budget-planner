b179cff2d10f41d578b83ef2c75cd6f18adfefe6
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-10 17:43:42 +0300

    Arc Exit Tween

--- graph.js
+++ graph.js
@@ -29,7 +29,10 @@ const update = (data) => {
     .data(pie(data))
 
   // handle the exit selection
-  paths.exit().remove();
+  paths.exit()
+    .transition().duration(750)
+    .attrTween('d', arcTweenExit)
+    .remove();
 
   // handle the current DOM path updates
   paths.attr('d', arcPath)
@@ -72,6 +75,15 @@ db.collection('expenses').onSnapshot(res => {
 
 const arcTweenEnter = (d) => {
   var i = d3.interpolate(d.endAngle, d.startAngle);
+
+  return function(t) {
+    d.startAngle = i(t);
+    return arcPath(d)
+  }
+}
+
+const arcTweenExit = (d) => {
+  var i = d3.interpolate(d.startAngle, d.endAngle);
 
   return function(t) {
     d.startAngle = i(t);
